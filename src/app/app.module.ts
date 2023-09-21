ad8a9acf49e116aa13f61781aeafcd23fef416d2
victor Ekundayo <victor.ekundayo@ietech.com.ng>
2023-09-21 19:44:31 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { TemplateComponent } from './template/template.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    TemplateComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
